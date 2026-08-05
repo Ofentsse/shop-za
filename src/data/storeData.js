9549b62007a4e9c08e211152276837375b715938
OfentsseDev <info.ofentsemosala@gmail.com>
2026-08-05 13:24:25 +0200

    mock data for development

---
+++ src/data/storeData.js
@@ -0,0 +1,111 @@
+export const newArrivals = [
+  {
+    id: 1,
+    name: 'T-shirt with Tape Details',
+    image: '/assets/tape-tshirt.png',
+    price: 120,
+    rating: 4.5,
+  },
+  {
+    id: 2,
+    name: 'Skinny Fit Jeans',
+    image: '/assets/skinny-jeans.png',
+    price: 240,
+    oldPrice: 260,
+    discount: 20,
+    rating: 3.5,
+  },
+  {
+    id: 3,
+    name: 'Checkered Shirt',
+    image: '/assets/checkered-shirt.png',
+    price: 180,
+    rating: 4.5,
+  },
+  {
+    id: 4,
+    name: 'Sleeve Striped T-shirt',
+    image: '/assets/striped-tshirt.png',
+    price: 130,
+    oldPrice: 160,
+    discount: 30,
+    rating: 4.5,
+  },
+]
+
+export const topSelling = [
+  {
+    id: 5,
+    name: 'Vertical Striped Shirt',
+    image: '/assets/vertical-shirt.png',
+    price: 212,
+    oldPrice: 232,
+    discount: 20,
+    rating: 5,
+  },
+  {
+    id: 6,
+    name: 'Courage Graphic T-shirt',
+    image: '/assets/courage-tshirt.png',
+    price: 145,
+    rating: 4,
+  },
+  {
+    id: 7,
+    name: 'Loose Fit Bermuda Shorts',
+    image: '/assets/bermuda-shorts.png',
+    price: 80,
+    rating: 3,
+  },
+  {
+    id: 8,
+    name: 'Faded Skinny Jeans',
+    image: '/assets/faded-jeans.png',
+    price: 210,
+    rating: 4.5,
+  },
+]
+
+export const dressStyles = [
+  {
+    id: 1,
+    title: 'Casual',
+    image: '/assets/casual.png',
+  },
+  {
+    id: 2,
+    title: 'Formal',
+    image: '/assets/formal.png',
+  },
+  {
+    id: 3,
+    title: 'Party',
+    image: '/assets/party.png',
+  },
+  {
+    id: 4,
+    title: 'Gym',
+    image: '/assets/gym.png',
+  },
+]
+
+export const testimonials = [
+  {
+    id: 1,
+    name: 'Sarah M.',
+    text:
+      "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+  },
+  {
+    id: 2,
+    name: 'Alex K.',
+    text:
+      'Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable.',
+  },
+  {
+    id: 3,
+    name: 'James L.',
+    text:
+      "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection is on-point with the latest trends.",
+  },
+]
